199b12063ebaae78ec86ab66ee724971e55d9016
arimbimn <arimbimega1@gmail.com>
2023-11-29 13:47:02 +0700

    add git ignore

--- application/logs/log-2023-11-27.php
+++ application/logs/log-2023-11-27.php
@@ -4,3 +4,5 @@ ERROR - 2023-11-27 14:07:24 --> Severity: Warning --> mysqli::real_connect(): (H
 ERROR - 2023-11-27 14:07:24 --> Unable to connect to the database
 ERROR - 2023-11-27 14:19:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'db_boco' D:\xampp2\htdocs\web-trial\system\database\drivers\mysqli\mysqli_driver.php 203
 ERROR - 2023-11-27 14:19:33 --> Unable to connect to the database
+ERROR - 2023-11-27 14:37:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'bocorocco_web' C:\xampp\htdocs\web\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2023-11-27 14:37:14 --> Unable to connect to the database
