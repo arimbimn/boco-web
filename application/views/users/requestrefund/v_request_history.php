e9879434043969d9d36ceb9a860759f796192257
arimbimn <arimbimega1@gmail.com>
2023-12-05 10:26:20 +0700

    change view request

--- application/views/users/requestrefund/v_request_history.php
+++ application/views/users/requestrefund/v_request_history.php
@@ -10,9 +10,9 @@
       <div class="col-sm-6">
         <nav aria-label="breadcrumb" class="theme-breadcrumb">
           <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?= base_url('/') ?>"><?=lang('beranda')?></a></li>
-                <li class="breadcrumb-item"><a href="<?= base_url('/users') ?>"><?=lang('my_account')?></a></li>
-                <li class="breadcrumb-item active" aria-current="page"><?= lang('title_moneyback_user') ?></li>
+            <li class="breadcrumb-item"><a href="<?= base_url('/') ?>"><?= lang('beranda') ?></a></li>
+            <li class="breadcrumb-item"><a href="<?= base_url('/users') ?>"><?= lang('my_account') ?></a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?= lang('title_moneyback_user') ?></li>
           </ol>
         </nav>
       </div>
@@ -29,9 +29,9 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-3">
-      <div class="account-sidebar"><a class="popup-btn"> <i class="fa fa-bars"></i> <?= lang('MyAccount') ?></a></div>
-        <div class="dashboard-left">
-          <div class="collection-mobile-back"><span class="filter-back"><i class="fa fa-angle-left" aria-hidden="true"></i><?=lang('back')?></span></div>
+        <div class="account-sidebar w-50 rounded-full font-normal bg-[#cd212a] capitalize text-white text-center !text-xs"><a class="popup-btn"> <i class="fa fa-bars"></i> <?= lang('MyAccount') ?></a></div>
+        <div class="dashboard-left rounded-md bg-white">
+          <div class="collection-mobile-back"><span class="filter-back"><i class="fa fa-angle-left" aria-hidden="true"></i><?= lang('back') ?></span></div>
           <div class="block-content">
             <?php $this->load->view('users/v_menu_user') ?>
           </div>
@@ -39,7 +39,7 @@
       </div>
       <div class="col-lg-9">
         <div class="dashboard-right">
-          <div class="dashboard">
+          <div class="dashboard bg-white rounded-md">
             <div class="page-title">
               <h2><?= lang('title_moneyback_user') ?></h2>
             </div>
@@ -54,7 +54,7 @@
                       <li class="nav-item"><a class="nav-link" id="top-home-tab" href="<?= base_url('requestrefund') ?>" role="tab" aria-selected="true"><?= lang('new_moneyback_user') ?></a>
                         <div class="material-border"></div>
                       </li>
-                      <li class="nav-item"><a class="nav-link active" id="profile-top-tab" href="#" role="tab" aria-selected="false"><?=lang('history_exchange_user')?></a>
+                      <li class="nav-item"><a class="nav-link active" id="profile-top-tab" href="#" role="tab" aria-selected="false"><?= lang('history_exchange_user') ?></a>
                         <div class="material-border"></div>
                       </li>
                     </ul>
@@ -64,7 +64,7 @@
                         <table class="table">
                           <thead>
                             <tr>
-                              <th scope="col"><?=lang('invoice_order')?></th>
+                              <th scope="col"><?= lang('invoice_order') ?></th>
                               <th scope="col"><?= lang('item_code_exchange_user') ?></th>
                               <th scope="col"><?= lang('alasan_exchange_user') ?></th>
                             </tr>
